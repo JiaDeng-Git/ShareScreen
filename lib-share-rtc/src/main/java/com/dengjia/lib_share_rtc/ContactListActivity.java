debf339426e0f02ecef1205d5279468d80c52bf5
JiaDeng <280858922@qq.com>
2020-05-05 22:03:19 +0800

    modify

---
+++ lib-share-rtc/src/main/java/com/dengjia/lib_share_rtc/ContactListActivity.java
@@ -0,0 +1,16 @@
+package com.dengjia.lib_share_rtc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+
+    }
+}
